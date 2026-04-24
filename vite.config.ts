2379c92aed3e3fb0fb5f03abe92226a721684a30
GustavoCunh4 <luiz.gustavo.cunha2003@hotmail.com>
2026-04-24 18:29:47 -0300

    chore: remove GitHub Pages deploy and update clients

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   root: '.',
   publicDir: 'public',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
     emptyOutDir: true,
     assetsDir: 'assets',
     rollupOptions: {
